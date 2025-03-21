ebdb6b7429fff08b7950b8d54b9a31b53ff47e6a
Sweater <dev@sweater.agency>
2025-03-21 16:06:51 +0100

    back-up

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const primaryTexts = document.querySelectorAll('.primary-text');
 const startUpHidden = document.querySelectorAll('p:not(.primary-text)');
 const imageContent = document.querySelector('.media-cont');
 const mediaContStartUp = document.querySelectorAll('.media-cont-start-up');
-
+const bodycopy = document.querySelectorAll('.bodycopy');
 const mobileQuery = matchMedia('(max-width: 750px)');
 const desktopQuery = matchMedia('(min-width: 751px)');
 
@@ -32,10 +32,10 @@ const showContent = (id) => {
 const handleLinkClick = (e) => {
     const targetId = e.target.id + "-content";
 
-    if (currentlyOpenContentId === targetId) { 
-        hideAllContent(); 
-        currentlyOpenContentId = null; 
-    } else { 
+    if (currentlyOpenContentId === targetId) {
+        hideAllContent();
+        currentlyOpenContentId = null;
+    } else {
         mobileStartUp();
         showContent(targetId);
         currentlyOpenContentId = targetId;
@@ -44,7 +44,7 @@ const handleLinkClick = (e) => {
 
 const handleCloseClick = () => {
     hideAllContent();
-    currentlyOpenContentId = null; 
+    currentlyOpenContentId = null;
     closeButton.classList.add("hidden");
 };
 
@@ -55,28 +55,66 @@ const mobileStartUp = () => {
 };
 
 // -------- desktop animation --------
+
+let desktopStartScreen = true;
 const beforeClick = () => {
+
+    gsap.set( mediaContStartUp, {opacity: 1});
+
     mediaContStartUp.forEach(element => element.classList.remove("hidden"));
 
     if (mobileQuery.matches) { // Mobile
         links.forEach(link => link.classList.add("white-text"));
     } else if (desktopQuery.matches) { // Desktop
-        primaryTexts.forEach(text => text.classList.add("white-text"));
+        gsap.set(".primary-text" , {color: "white"});
+        // primaryTexts.forEach(text => text.classList.add("white-text"));
         startUpHidden.forEach(element => element.classList.add("hidden"));
         imageContent.classList.add("hidden");
     }
 };
 
+
 const desktopStartUp = () => {
     if (desktopQuery.matches) { // Desktop only
+
         primaryTexts.forEach(text => text.classList.remove("white-text"));
         startUpHidden.forEach(element => element.classList.remove("hidden"));
         imageContent.classList.remove("hidden");
-        mediaContStartUp.forEach(element => element.classList.add("hidden"));
+
+        if (desktopStartScreen) {
+            desktopStartScreen = false;
+            gsapSetUp();
+        }
     }
 };
 
-// -------- Initialization --------
+const gsapSetUp = () => {
+    // Create a timeline for smooth control
+    const tl = gsap.timeline();
+
+    tl
+
+    .to(".primary-text", {
+        duration: 0.5,
+        color: "black",
+    }, "start")
+    
+    .to(mediaContStartUp,{
+        duration: 0.5,
+        opacity: 0,
+        ease: "power3.inOut"
+    }, "start")
+    .from(bodycopy, {
+        duration: 2,
+        rotation: 20,
+        opacity: 0,
+        stagger: 0.2,
+        ease: "power3.inOut"
+    }, "-=0.5")
+
+};
+
+// -------- init --------
 const init = () => {
     console.log("Links found:", links.length);
     hideAllContent();
