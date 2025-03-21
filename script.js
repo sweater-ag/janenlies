becfdcfcaad659c5a9c9168e6d589bb339686840
Sweater <dev@sweater.agency>
2025-03-21 11:42:56 +0100

    back-up

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 const links = document.querySelectorAll('.link');
 const closeButton = document.querySelector('.button-close');
 const contentContainers = document.querySelectorAll('.content-cont');
+const primaryTexts = document.querySelectorAll('.primary-text');
+// const startUpHidden = document.querySelectorAll('body *:not(.primary-text):not(.media-cont-start-up:not(.media-cont-start-up img))');
+const startUpHidden = document.querySelectorAll('p:not(.primary-text)');
+const imageContent = document.querySelector('.media-cont');
+const mediaContStartUp = document.querySelector('.media-cont-start-up');
 
+
+// -------- mobile content interaction --------
 const hideAllContent = () => {
     contentContainers.forEach(container => {
         container.classList.add("hidden");
@@ -26,11 +33,45 @@ const handleCloseClick = () => {
     closeButton.classList.add("hidden");
 };
 
+// -------- mobile animation --------
+
+//-------- desktop animation --------
+
+const desktopBeforeClick = () => {
+    primaryTexts.forEach(text => {
+        text.classList.add("white-text");
+    });
+
+    startUpHidden.forEach(element => {
+        element.classList.add("hidden");
+    });
+
+    imageContent.classList.add("hidden");
+
+     mediaContStartUp.classList.remove("hidden");
+}
+
+const desktopStartUp = () => {
+    primaryTexts.forEach(text => {
+        text.classList.remove("white-text");
+    });
+
+    startUpHidden.forEach(element => {
+        element.classList.remove("hidden");
+    });
+    imageContent.classList.remove("hidden");
+
+    mediaContStartUp.classList.add("hidden");
+
+}
+
 const init = () => {
     console.log("Links found:", links.length);
     hideAllContent();
     links.forEach(link => link.addEventListener("click", handleLinkClick));  // Use the handler function directly
     closeButton.addEventListener("click", handleCloseClick);
+    desktopBeforeClick();
+    window.addEventListener("click", desktopStartUp);
 };
 
 init();
