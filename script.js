411ee48cb9fdfedf4eba90d72268e26d353c45d6
Sweater <dev@sweater.agency>
2025-03-24 12:08:34 +0100

    redoing the structure

--- script.js
+++ script.js
@@ -1,49 +1,41 @@
 gsap.registerPlugin(ScrollTrigger);
 
-const links = document.querySelectorAll('.link');
+// mobile 
+const linksMobile = document.querySelectorAll('#mobile-layout .link');
 const closeButton = document.querySelector('.button-close');
-const contentContainers = document.querySelectorAll('.content-cont');
-const primaryTexts = document.querySelectorAll('.primary-text');
-const startUpHidden = document.querySelectorAll('p:not(.primary-text)');
-const imageContent = document.querySelector('.media-cont');
-const mediaContStartUp = document.querySelectorAll('.media-cont-start-up');
-const bodycopy = document.querySelectorAll('.bodycopy');
-const mobileQuery = matchMedia('(max-width: 750px)');
-const desktopQuery = matchMedia('(min-width: 751px)');
-
-//scroll text 
-
-// gsap.fromTo(".overflow-text", 
-//     { y: 0 },
-//     {
-//         y: -100, // Move the text downwards
-//         ease: "power1.out",
-//         scrollTrigger: {
-//             trigger: ".overflow-container",
-//             start: "20% top", // Start animation when 20% of the element is visible
-//             end: '+=100%',
-//             markers: true,
-            
-//             scrub: true // Smooth scrolling effect
-//         }
-//     }
-// );
+const contentContainersMobile = document.querySelectorAll('.content-cont');
+const startUpImgMobile = document.querySelector('#mobile-layout .media-cont-start-up');
 
 
+//desktop 
+// const startUpHidden = document.querySelectorAll('#desktop-layout p:not(.primary-text)');
+const pictureDesktopContent = document.querySelector('.media-cont');
+const startUpImgDesktop = document.querySelector('#desktop-layout .media-cont-start-up');
+const mainGridDesktop = document.querySelector('#desktop-layout .main-grid');
+const desktopText = document.querySelectorAll('#desktop-layout .desktop-text');
+const primaryTextsDesktop = document.querySelectorAll('#desktop-layout .primary-text');
+const overflowContainerDesktop = document.querySelectorAll('#desktop-layout .overflow-container');
+
+const mobileQuery =  window.matchMedia('(max-width: 750px)');
+const desktopQuery =  window.matchMedia('(min-width: 751px)');
+
+
+
+console.log(overflowContainerDesktop);
 
 // -------- mobile content interaction --------
 
 let currentlyOpenContentId = null;
 
-const hideAllContent = () => {
-    contentContainers.forEach(container => {
+const hideMobileOpenContent = () => {
+    contentContainersMobile.forEach(container => {
         container.classList.add("hidden");
     });
     closeButton.classList.add("hidden");
 };
 
 const showContent = (id) => {
-    hideAllContent();
+    hideMobileOpenContent();
     const contentToShow = document.getElementById(id);
     if (contentToShow) contentToShow.classList.remove("hidden");
     closeButton.classList.remove("hidden");
@@ -53,7 +45,7 @@ const handleLinkClick = (e) => {
     const targetId = e.target.id + "-content";
 
     if (currentlyOpenContentId === targetId) {
-        hideAllContent();
+        hideMobileOpenContent();
         currentlyOpenContentId = null;
     } else {
         mobileStartUp();
@@ -63,119 +55,128 @@ const handleLinkClick = (e) => {
 };
 
 const handleCloseClick = () => {
-    hideAllContent();
+    hideMobileOpenContent();
     currentlyOpenContentId = null;
     closeButton.classList.add("hidden");
 };
 
-// -------- mobile animation --------
-const mobileStartUp = () => {
-    links.forEach(link => link.classList.remove("white-text"));
-    mediaContStartUp.forEach(element => element.classList.add("hidden"));
-};
+mobileDefault = () => {
+    startUpImgMobile.classList.remove("hidden");
+    // linksMobile.forEach(link => link.classList.add("white-text"));
+    gsap.set(linksMobile, { color: "white" });
+    linksMobile.forEach(link => link.classList.remove("hidden"));
+}
 
-// -------- desktop animation --------
+const mobileStartUp = () => {
+    startUpImgMobile.classList.add("hidden");
+
+    gsap.to(linksMobile, {
+        duration: 0.1,
+        color: "black",
+    });
+}
+
+
+// -------- desktop content interaction --------
 
 let desktopStartScreen = true;
 
 
-
-const beforeClick = () => {
-
-    mediaContStartUp.forEach(element => element.classList.remove("hidden"));
-
-    if (mobileQuery.matches) { // Mobile
-        links.forEach(link => link.classList.add("white-text"));
-    } else if (desktopQuery.matches) { 
-        // gsap.set([".overflow-container", startUpHidden], { opacity: 0, y: 70 });
-        gsap.set([".overflow-container"], { opacity: 0, y: 10 });
-        gsap.set(startUpHidden, { opacity: 0 });
-        gsap.set(imageContent, { opacity: 0, y: 80 });
+desktopDefault = () => {
+    desktopText.forEach(element => element.classList.add("hidden"));
+    startUpImgDesktop.classList.remove("hidden");
+    mainGridDesktop.style.cursor = "pointer";
 
 
-        gsap.set(mediaContStartUp, { opacity: 1 });
-        // gsap.set( bodycopy, {opacity: 0});
-        gsap.set(".primary-text", { color: "white" });
-        // gsap.set(startUpHidden, {opacity: 0});
-
-        // primaryTexts.forEach(text => text.classList.add("white-text"));
-        startUpHidden.forEach(element => element.classList.add("hidden"));
-        imageContent.classList.add("hidden");
-    }
-};
-
+    gsap.set(primaryTextsDesktop, { color: "white" });
+    gsap.set(pictureDesktopContent, { opacity: 0, y: 80 });
+    gsap.set(startUpImgDesktop, { opacity: 1 });
+    gsap.set(desktopText , {opacity: 0});
+    // gsap.set(overflowContainerDesktop, { opacity: 0 });
+}
 
 const desktopStartUp = () => {
-    if (desktopQuery.matches) { // Desktop only
-
-        // primaryTexts.forEach(text => text.classList.remove("white-text"));
-        startUpHidden.forEach(element => element.classList.remove("hidden")); //2 texts
-
-        imageContent.classList.remove("hidden");
-
-        if (desktopStartScreen) {
-            desktopStartScreen = false;
-            gsapSetUp();
-        }
+    if (desktopStartScreen) {
+        desktopText.forEach(element => element.classList.remove("hidden"));
+        mainGridDesktop.style.cursor = "default";
+        desktopStartScreen = false;
+        gsapSetUp();
     }
 };
+
 
 const gsapSetUp = () => {
     const tl = gsap.timeline();
     tl
-        .to(".primary-text", { //titles
+        .to(primaryTextsDesktop, { //titles
             duration: 1,
             color: "black",
         }, "start")
-        .to(mediaContStartUp, { //image black
+        .to(startUpImgDesktop, { //image black
             duration: 1,
             opacity: 0,
             ease: "power3.inOut"
         }, "start")
 
-        .to(startUpHidden,{
-            opacity: 1,
-            duration: 1,
-            
-            ease: "power3.inOut"
-        }, "start")
-
-        .to(imageContent, { //garden
+        .to(pictureDesktopContent, { //garden
             duration: 1,
             opacity: 1,
             y: 0,
             ease: "power3.inOut"
         },)
-        .to(".overflow-container", {  
+        .to(desktopText , {  
             duration: 0.8,
             opacity:1,
             stagger: 0.8,
             ease: "sine.out",
-        },"<=+0.8" )
-
-        .to(".overflow-container", {  
-            duration: 0.5,
-            y: 0,
-            stagger: 0.9,
-            ease: "sine.out",
-        }, "<") 
-
+        })
+        .to(overflowContainerDesktop, {
+            duration: 0.1,
+            opacity: 1,
+            ease: "power3.inOut"
+        }, "start+=0.9")
         
 
 };
 
-// -------- init --------
-document.addEventListener("scroll", () => {
-    console.log("scrolling");
-  });
+// -------- else --------
 
+const handleScroll = (e) => {
+    e.preventDefault();
+    console.log("scrolling", e);
+    e.stopPropagation();
+} 
+
+
+
+const handleResize = (e) => {
+    console.log("resize", e);
+    if (e.matches) {
+        console.log("mobile");
+    } else {
+        console.log("desktop");
+        beforeClick();
+    }
+}
+
+const beforeClick = () => {
+    desktopDefault();
+    mobileDefault();
+
+    hideMobileOpenContent(); 
+};
+
+
+// -------- init --------
 const init = () => {
-    console.log("Links found:", links.length);
-    hideAllContent();
-    links.forEach(link => link.addEventListener("click", handleLinkClick));
-    closeButton.addEventListener("click", handleCloseClick);
     beforeClick();
-    window.addEventListener("click", desktopStartUp);
+
+    closeButton.addEventListener("click", handleCloseClick);
+    
+    linksMobile.forEach(link => link.addEventListener("click", handleLinkClick));
+    mainGridDesktop.addEventListener("click", desktopStartUp);
+
+    mobileQuery.addEventListener("change", handleResize);
 };
 
 init();
